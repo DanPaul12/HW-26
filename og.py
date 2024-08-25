1ebb2c0c60eeec0454afab9652ea7e10b1f68a1b
DanPaul12 <dan.paul.schechter@gmail.com>
2024-08-25 11:20:50 -0700

    hw26

--- og.py
+++ og.py
@@ -1,42 +1,54 @@
 from flask import Flask, jsonify
 from flask_marshmallow import Marshmallow
 from marshmallow import fields
-import mysql.connector
+from sqlconnect import get_db_connection
 from mysql.connector import Error
-
 
 app = Flask(__name__)
 ma = Marshmallow(app)
 
-def get_db_connection():
-    db_name = "fitness_center_db"
-    user = "root"
-    host = "localhost"
-    password = "thegoblet2"
-    try:
-        conn = mysql.connector.connect(
-            database = db_name,
-            user = user,
-            hot = host,
-            password = password
-        )
-        if conn is not None:
-            print("Connection succcessful")
-    except Error as e:
-        print({e})
+class MemberSchema(ma.Schema):
+    name = fields.String(required = True)
+    age = fields.String(required = True)
+
+    class Meta:
+        fields = ("name", "age")
+
+member_schema = MemberSchema()
+members_schema = MemberSchema(many = True)
+
+
+
 
 @app.route('/')
 def home():
     return"Welcome"
 
+@app.route('/members', methods = ["GET"])
+def get_members():
+    try:
+        conn = get_db_connection()
+        if conn is None:
+            return jsonify({"error":"Database connection failed"}), 500
+        cursor = conn.cursor(dictionary = True)
+
+        query = "SELECT * from Members"
+
+        cursor.execute(query)
+
+        members = cursor.fetchall
+
+        return members_schema.jsonify(members)
+    except Error as e:
+        print({e})
+    finally:
+        if conn and conn.is_connected():
+            cursor.close()
+            conn.close()
+
 if __name__ == "__main__":
     app.run(debug= True)
 
 
-def get_member(id):
-    conn = get_db_connection()
-    if conn is None:
-        return jsonify({"error":"Database connection failed"}), 500
-    cursor = conn.cursor()
 
     
---
+++ sqlconnect.py
@@ -0,0 +1,21 @@
+import mysql.connector
+from mysql.connector import Error
+
+def get_db_connection():
+    db_name = "fitness_center_db"
+    user = "root"
+    host = "localhost"
+    password = "thegoblet2"
+    try:
+        conn = mysql.connector.connect(
+            database = db_name,
+            user = user,
+            hot = host,
+            password = password
+        )
+        if conn is not None:
+            print("Connection succcessful")
+    except Error as e:
+        print({e})
+
+get_db_connection()
