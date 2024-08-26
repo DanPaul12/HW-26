6f693c9c2d8491b6b661511b8ef8dda1d158e898
DanPaul12 <dan.paul.schechter@gmail.com>
2024-08-25 21:01:48 -0700

    hw26

--- og.py
+++ og.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_marshmallow import Marshmallow
-from marshmallow import fields
+from marshmallow import fields, ValidationError
 import mysql.connector
 from mysql.connector import Error
 
@@ -8,11 +8,12 @@ app = Flask(__name__)
 ma = Marshmallow(app)
 
 class MemberSchema(ma.Schema):
+    id = fields.Int(dump_only = True)
     name = fields.String(required = True)
     age = fields.String(required = True)
 
     class Meta:
-        fields = ("name", "age")
+        fields = ("id", "name", "age")
 
 member_schema = MemberSchema()
 members_schema = MemberSchema(many = True)
@@ -32,6 +33,7 @@ def get_db_connection():
         )
         if conn is not None:
             print("Connection succcessful")
+            return conn
     except Error as e:
         print({e})
 
@@ -47,21 +49,78 @@ def get_members():
         conn = get_db_connection()
         if conn is None:
             return jsonify({"error":"Database connection failed"}), 500
-        cursor = conn.cursor(dictionary = True)
+        cursor = conn.cursor(dictionary = True, buffered = True)
 
         query = "SELECT * from Members"
 
         cursor.execute(query)
 
-        members = cursor.fetchall
+        members = cursor.fetchall()
 
         return members_schema.jsonify(members)
     except Error as e:
         print({e})
+        return jsonify({"error": 'internal server error'}), 500
     finally:
         if conn and conn.is_connected():
             cursor.close()
             conn.close()
+
+
+@app.route('/members/<int:id>', methods = ["GET"])
+def get_member(id):
+    try:
+        conn = get_db_connection()
+        if conn is None:
+            return jsonify({"error":"Database connection failed"}), 500
+        
+        cursor = conn.cursor(dictionary = True, buffered = True)
+        cursor.execute("SELECT * from Members where id = %s", (id,))
+        member = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+        if member:
+            return member_schema.jsonify(member)
+        else:
+            return jsonify({'error':"member not found"}), 404
+    except Error as e:
+        print({e})
+        return jsonify({"error": 'internal server error'}), 500
+    finally:
+        if conn and conn.is_connected():
+            cursor.close()
+            conn.close()
+
+
+@app.route('/members', methods = ["POST"])
+def add_member():
+    try:
+       member_data = member_schema.load(request.json)
+    except ValidationError as e:
+        print({e})
+    try:
+        conn = get_db_connection()
+        if conn is None:
+            return jsonify({"error":"Database connection failed"}), 500
+        member = member_data["name"], member_data['age']
+        
+        cursor = conn.cursor(buffered = True)
+
+        query = "INSERT INTO Members  (name, age) Values (%s, %s)"
+
+        cursor.execute(query, member)
+        conn.commit()
+        return jsonify({'message': 'member added successfully'}), 201
+    except Error as e:
+        print({e})
+        return jsonify({"error": 'internal server error'}), 500
+    finally:
+        if conn and conn.is_connected():
+            cursor.close()
+            conn.close()
+    
+    
 
 if __name__ == "__main__":
     app.run(debug= True)
